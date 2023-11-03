4096c0b0eb3581bd74e259d7fc180d5bad867a8d
David-Jesse <djesse351@gmail.com>
2023-11-02 17:41:19 -0700

    added the Aboutpage, feedbackform, feedbackcontext, RatingSelect and some components i tagged share: cause i will be reusing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {v4 as uuidv4} from 'uuid';
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
+
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if(window.confirm('Are you sure about this action?')){
@@ -14,14 +25,26 @@ function App() {
   }
 
   return (
-    <>
-      <Header />
-      <div className="container">
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-      </div>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={
+              <>
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+              </>
+            }>
 
-    </>
+            </Route>
+
+            <Route path='/about' element={<AboutPage />}/>
+          </Routes>
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
